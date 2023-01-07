3378704bb1d44fbca824d44db3b60b171be01198
ManishSorte <mksorte1798@gmail.com>
2023-01-07 04:21:42 -0800

    Implemented Resilience4j for Order and Inventory service

--- order-service/src/main/java/com/ecommerce/Orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/ecommerce/Orderservice/controller/OrderController.java
@@ -3,6 +3,9 @@ package com.ecommerce.Orderservice.controller;
 import com.ecommerce.Orderservice.exception.ApiResponse;
 import com.ecommerce.Orderservice.payload.OrderDto;
 import com.ecommerce.Orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +19,9 @@ public class OrderController {
     private OrderService orderService;
 
     @PostMapping("/create")
+    @CircuitBreaker(name = "inventoryBreaker", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name="inventoryTimeOut")
+    @Retry(name="inventoryRetry")
     public ResponseEntity<OrderDto> placeOrder(@RequestBody OrderDto orderDto) {
         OrderDto placedOrder = this.orderService.placeOrder(orderDto);
         return new ResponseEntity<>(placedOrder, HttpStatus.CREATED);
@@ -31,5 +37,12 @@ public class OrderController {
     public ResponseEntity<ApiResponse> deleteOrderById(@PathVariable Integer orderId) {
         this.orderService.deleteOrder(orderId);
         return new ResponseEntity<>(new ApiResponse("Order deleted successfully", true), HttpStatus.ACCEPTED);
+    }
+    public ResponseEntity<OrderDto> fallbackMethod(OrderDto orderDto, Exception runtimeException) {
+        OrderDto failedOrderDto = new OrderDto();
+        failedOrderDto.setId(0);
+        failedOrderDto.setOrderNumber("Dummy Number");
+        failedOrderDto.setPrice("Dummy Price");
+        return new ResponseEntity<>(failedOrderDto,HttpStatus.GATEWAY_TIMEOUT);
     }
 }
